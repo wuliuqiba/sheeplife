56176d8732b270979a63505aa5f29c8f46d98d13
wzq <13131355228@163.com>
2023-02-13 21:51:27 +0800

    first commit：项目创建

---
+++ src/main/java/com/comeon/sheeplife/SheeplifeApplication.java
@@ -0,0 +1,13 @@
+package com.comeon.sheeplife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheeplifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SheeplifeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/comeon/sheeplife/SheeplifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comeon.sheeplife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheeplifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
